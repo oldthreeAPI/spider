7efba55fa35dc817f1c0fb49a66ebd30d4207c0f
oldthree <50625656+oldthreeAPI@users.noreply.github.com>
2021-03-09 22:10:25 +0800

    电商网站

---
+++ GIS_date/charging_adress/online_Retailers/JD_test.py
@@ -0,0 +1,27 @@
+import requests
+
+class jd_spider():
+    """
+    https://search.jd.com/Search?keyword=手机&suggest=1.his.0.0&wq=%E6%89%8B%E6%9C%BA&page=3
+    直接可以请求成功
+    """
+    def __init__(self):
+        pass
+
+    def sturctrue_url(self):
+        """构造url列表"""
+        pass
+
+    def parse_url(self):
+        """发送请求"""
+        pass
+
+    def save_date(self):
+        """保存数据"""
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    pass
